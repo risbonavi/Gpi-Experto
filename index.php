4fc35252618233422867c598762e519ad25b99e1
Neftali Madariaga Castro <tali-tali@openmailbox.org>
2014-12-04 15:50:33 -0300

    pequeños cambios

--- index.php
+++ index.php
@@ -25,13 +25,9 @@ session_start();
 	
 	<div id='cssmenu'>
 <ul>
-<<<<<<< HEAD
-   <li><a href='index.php'>Inicio</a></li>
-   <li><a href='php/cliente.php'>Postulante</a></li>
-=======
+
     <li><a href='index.php'>Inicio</a></li>
    <li><a href='php/ingreso.php'>Cliente</a></li>
->>>>>>> origin/master
    <li><a href='php/gerente.php'>Gerente</a></li>
    
 </ul>
