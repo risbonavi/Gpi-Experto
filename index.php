b6bcf9ec35f344140d42c1a17d98c75f87087cb3
Pedro <pdro.gonzalez@gmail.com>
2014-12-04 15:16:39 -0300

    Agregada vista ingreso

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ session_start();
 	
 	<div id='cssmenu'>
 <ul>
-   <li><a href='index.php'>Inicio</a></li>
-   <li><a href='php/cliente.php'>Cliente</a></li>
+    <li><a href='index.php'>Inicio</a></li>
+   <li><a href='php/ingreso.php'>Cliente</a></li>
    <li><a href='php/gerente.php'>Gerente</a></li>
    
 </ul>
